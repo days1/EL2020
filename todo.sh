b5958b33d68c5c1be43d435dc4ab963f7fbae1cb
days1 <sanchoday@gmail.com>
2020-02-25 15:29:30 -0500

    Added the 'AddToList' function

--- todo.sh
+++ todo.sh
@@ -13,6 +13,10 @@ getList(){
 	done < "$TODO_LIST"
 }
 
+addToList(){
+	echo $taskToDo >> todo.txt
+}
+
 PS3="Enter in one of the following commands: "
 
 select command in DisplayList AddToList RemoveFromList Exit
@@ -23,6 +27,16 @@ do
 			getList
 			echo "--------------"
 			;;
+		AddToList)
+			echo "--------------"
+			echo "Enter in what you need to do later"
+			read taskToDo
+			echo $taskToDo
+			addToList $taskToDo
+			echo $taskToDo has been added to the TO-DO List
+			echo "--------------"
+			;;
+		RemoveFromList)
 		Exit)
 			echo "Goodbye!"
 			exit 0
