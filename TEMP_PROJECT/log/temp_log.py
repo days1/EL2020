c5b77938b5b76819f5e42cffb55c859150d83025
days1 <sanchoday@gmail.com>
2020-03-04 18:45:58 -0500

    Added temp_log.py that outputs templog.csv

---
+++ TEMP_PROJECT/log/temp_log.py
@@ -0,0 +1,50 @@
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import signal
+import time
+import os
+
+redPin = 27
+tempPin = 17
+touchPin = 26
+
+tempSensor = Adafruit_DHT.DHT11
+
+blinkDur = .1
+blinkTime = 7
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(redPin, GPIO.OUT)
+GPIO.setup(touchPin, GPIO.IN)
+
+def oneBlink(pin):
+	GPIO.output(pin,True)
+	time.sleep(blinkDur)
+	GPIO.output(pin,False)
+	time.sleep(blinkDur)
+
+def readF(tempPin):
+	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
+	temperature = temperature * 9/5.0 +32
+	if humidity is not None and temperature is not None:
+		tempFahr = '{0:0.1f}*F'.format(temperature)
+	else:
+		print('Error Reading Sensor')
+	return tempFahr
+
+try:
+	with open("../log/templog.csv", "a") as log:
+		while True:
+			input_state = GPIO.input(touchPin)
+			if input_state == True:
+				for i in range (blinkTime):
+					oneBlink(redPin)
+				time.sleep(.2)
+				data = readF(tempPin)
+				print(data)
+				log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(data)))
+
+except KeyboardInterrupt:
+	os.system('clear')
+	print('Thanks for Blinking and Thinking')
+	GPIO.cleanup()
